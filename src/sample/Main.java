89b2cc9028d948bb50c03dec1c35f4373a648336
Nongluk04012538 <nongluk091@gmail.com>
2018-02-21 10:02:46 +0700

    Controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("School management System");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("School Mansgement System");
+        primaryStage.setScene(new Scene(root, 600, 390));
+
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
