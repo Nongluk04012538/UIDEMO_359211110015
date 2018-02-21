09981b85b97f686deb24762a57ec39f7449abe8d
Nongluk04012538 <nongluk091@gmail.com>
2018-02-21 10:35:32 +0700

    dbConnecttion

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -4,20 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class dbConnection {
-    private static final String SQCONN="jdbc:school.sqlite";
+import static java.lang.Class.forName;
 
-    public static Connection getconnection(){
+public class dbConnection {
+
+    private static final String SOCONN ="jdbc:sqlite:school.sqlite";
+
+    public static Connection getConnection(){
+
         try {
+
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(SQCONN);
-        }catch (ClassNotFoundException ex){
+            return DriverManager.getConnection(SOCONN);
+        }catch (ClassNotFoundException ex) {
             ex.printStackTrace();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
 
 
-    }//getconnection
-}//class
+
+
+    }
+
+
+
+}//getConection
